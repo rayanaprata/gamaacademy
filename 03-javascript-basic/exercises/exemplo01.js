d33bcb2cc513fb60a7d5aa43a092e5ced992d615
Rayana Prata <rayanaprata@uol.com.br>
2021-07-08 09:01:32 -0300

    exercicio1

---
+++ 03-javascript-basic/exercises/exemplo01.js
@@ -0,0 +1,13 @@
+/* Exemplo 01: Vamos criar variaveis usando let, const e var, atribuir valores, 
+identificar comportamentos e saídas quando tentarmos recuperar. */
+
+let a;
+var b;
+const c = 15;
+
+a = 5;
+b = 10;
+
+console.log(a)
+console.log(b)
+console.log(c)
