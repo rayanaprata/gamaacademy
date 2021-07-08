4812ea3ec190177dd210c3d4eeb928c1cc15365e
Rayana Prata <rayanaprata@uol.com.br>
2021-07-08 09:01:45 -0300

    exercicio2

---
+++ 03-javascript-basic/exercises/exemplo02.js
@@ -0,0 +1,10 @@
+/* Exemplo 02: Vamos criar variaveis usando let, const e var, atribuir strings e vamos concatenar e interpolar. */
+
+let strA;
+const strB = "const";
+var strC;
+
+strA = "let";
+strC = "var";
+
+console.log(`${strA} & ${strB} & ${strC}`);
