b182e069b6d4c2e4544298c1630e8a8b8f4b7b08
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-07-11 07:49:22 -0300

    operador ternario

---
+++ 03-javascript/exercises-js-advanced/operador-ternario.js
@@ -0,0 +1,15 @@
+// if...else if
+
+// if (idade > 18) {
+//   return "Maior de idade";
+// } else {
+//   return "Menor de idade";
+// }
+
+// idade >= 18 ? "Maior de idade" : "Menor de idade";
+
+function calculaIdade(idade) {
+  return idade >= 18 ? "Maior de idade" : "Menor de idade";
+}
+
+console.log(calculaIdade(19));
