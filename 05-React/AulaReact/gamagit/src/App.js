e258329aa8572bd0f7b839b8dbfd0bab941f2e69
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-07-13 07:50:08 -0300

    aula de react

--- 05-React/AulaReact/gamagit/src/App.js
+++ 05-React/AulaReact/gamagit/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function App(props) {
+  const [usuario, setUsuario] = useState("");
+
+  function handlePesquisa() {
+    console.log(usuario);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        className="usuarioInput"
+        placeholder="Usuário"
+        value={usuario}
+        onChange={(e) => setUsuario(e.target.value)}
+      />
+      <button type="button" onClick={handlePesquisa}>
+        Pesquisar
+      </button>
+    </>
   );
 }
 
